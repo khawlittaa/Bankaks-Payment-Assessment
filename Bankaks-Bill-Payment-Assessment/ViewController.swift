98af315641ae79fb4ab8d37a461d9b049e829a83
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2020-11-29 12:32:02 +0100

    initial commit

---
+++ Bankaks-Bill-Payment-Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankaks-Bill-Payment-Assessment
+//
+//  Created by khaoula hafsia on 29/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
