41c7eb9417d8db94b40386bd68f2f73ceed10b20
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2020-11-30 21:56:55 +0100

    setting up cells UIs

---
+++ Bankaks-Bill-Payment-Assessment/Views/InputTextCell.swift
@@ -0,0 +1,23 @@
+//
+//  InputTextCell.swift
+//  Bankaks-Bill-Payment-Assessment
+//
+//  Created by khaoula hafsia on 30/11/2020.
+//
+
+import UIKit
+
+class InputTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Bankaks-Bill-Payment-Assessment/Views/ServiceCell.swift
@@ -0,0 +1,23 @@
+//
+//  ServiceCell.swift
+//  Bankaks-Bill-Payment-Assessment
+//
+//  Created by khaoula hafsia on 30/11/2020.
+//
+
+import UIKit
+
+class ServiceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
